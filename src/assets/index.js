66c14f41a8c0210a6b49b073168336f6811cfff7
nandan333642 <138384353+nandan333642@users.noreply.github.com>
2023-07-03 02:15:50 +0530

    updated sun2nd july

---
+++ src/assets/index.js
@@ -0,0 +1,20 @@
+export {default as concert1} from './flex/concert1.jpg';
+export {default as concert2} from './flex/concert2.jpg';
+export {default as concert3} from './flex/concert3.jpg';
+
+export {default as spo1} from './galleryspo/spo1.png';
+export {default as spo2} from './galleryspo/spo2.png';
+export {default as spo3} from './galleryspo/spo3.png';
+export {default as spo4} from './galleryspo/spo4.png';
+export {default as gallery} from './gallery.webp';
+export {default as school} from './DSC_0332.JPG';
+export {default as poster} from './poster.jpg';
+
+export {default as insta} from './icons/instagram.png';
+export {default as twitter} from './icons/twitter.png';
+export {default as fb} from './icons/facebook.png';
+export {default as yt} from './icons/youtube.png';
+
+export {default as westdance} from './gallery/DSC_0607.JPG';
+export {default as grooves} from './gallery/DSC_0555.JPG';
+export {default as pulse} from './gallery/DSC_0392.JPG';
